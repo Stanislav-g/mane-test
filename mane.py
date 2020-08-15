c85f7ee411a76310da598917334fc5c7de31b484
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-08-15 19:40:23 +0300

    Update mane.py

--- mane.py
+++ mane.py
@@ -8,18 +8,47 @@ import asyncio
 
 client = commands.Bot( command_prefix = '*')
 
-word = {}
-
-word_width = 24
-word_height = 8
-
 
 
 @client.event
 async def on_redy():
     print( 'Bot connected')
 
-  
+    
+@client.command()
+async def mine(ctx):    
+world = {}
+world_width = 24
+world_heirht = 8
+
+second_part = [0,1]
+third_part = [1,2]
+
+def generate_world():
+    for x in range(world_height):
+        if x <= 2:
+            for x in range(world_height):
+                await ctx.send(f'0', end = '')
+            await ctx.send()
+        elif x <= 3:
+            for x in range(world_width):
+                await ctx.send(random.choice(second_part), end = '')
+            await ctx.send()
+        elif x <= 4:
+            for x in range(world_width):
+                await ctx.send(1, end = '')
+            await ctx.send()
+        elif x <= 5:
+            for x in range(world_width):
+                await ctx.send(random.choice(third_part), end = '')
+            await ctx.send()
+        elif x <= 7:
+            for x in range(world_width):
+                await ctx.send(f'2', end = '')
+            await ctx.send()
+
+generate_world()
+ 
 
     
     
